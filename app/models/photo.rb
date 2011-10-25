77eefaa1532639392355ac8a82b8416df50501cd
Anders Westrup <anders@westrup.se>
2011-10-25 21:29:29 +0200

    skipping flagged photos

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,6 +3,7 @@ class Photo < ActiveRecord::Base
   belongs_to :event
   default_scope order('exposure_time')
   default_scope :conditions => 'rating <> -1' # Skip rating = denied
+  default_scope :conditions => 'flags = 0' # Skip flagged photos
   default_scope :conditions => 'event_id <> -1' # Skip photos in trash can
   default_scope :conditions => ['id not in (?)', Tag.find_by_name("Hidden").photos_ids] # Skip photos with "Hidden" tag
   
